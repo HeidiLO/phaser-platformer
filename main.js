ecc283a8505d6e50415aa6d49886d99e73f38cce
Heidi <osburnhl@s.dcsdk12.org>
2023-10-30 16:23:33 -0600

    30-10-2023

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 import './style.css'
 import Phaser from 'phaser'
-/**@type {Phaser.Core.Config}*/
+class MainScene extends Phaser.Scene{
+  constructor(){
+    super("main-scene")
+  }
+  preload(){
+    this.load.atlas("Robot", "Robot.png", "Robot.json");
+  }
+  create(){
+    const {height, width} = this.scale;
+    let player = this.add.sprite(width/2, height/2, "Robot")
+  }
+  update(){}
+}
+/**@type {Phaser.Types.Core.GameConfig}*/
 const config = {
   type: Phaser.WEBGL,
   width: 400,
   height: 400,
+  scene: [MainScene]
 };
 const game = new Phaser.Game(config)
