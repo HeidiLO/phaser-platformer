da2b030fd23f2134c0ff271dd30525977658b471
Heidi <osburnhl@s.dcsdk12.org>
2023-11-01 16:08:21 -0600

    1-11-2023

--- main.js
+++ main.js
@@ -12,8 +12,48 @@ class MainScene extends Phaser.Scene {
     let player = this.add.sprite(width / 2, height / 2, "Robot");
     player.anims.create({
       key: "run",
-      frames: player.anims.generateFrameNames()
-    })
+      frames: player.anims.generateFrameNames("Robot", {
+        start: 0,
+        end: 2,
+        prefix: "character_robot_run",
+        suffix: ".png"
+      }),
+      frameRate: 12,
+      repeat: -1,
+    });
+   // player.play("run")
+  }
+  create() {
+    const { height, width } = this.scale;
+    let player = this.add.sprite(width / 2, height / 2, "Robot");
+    player.anims.create({
+      key: "walk",
+      frames: player.anims.generateFrameNames("Robot", {
+        start: 0,
+        end: 7,
+        prefix: "character_robot_walk",
+        suffix: ".png"
+      }),
+      frameRate: 12,
+      repeat: -1,
+    });
+    player.play("walk")
+  }
+  create() {
+    const { height, width } = this.scale;
+    let player = this.add.sprite(width / 2, height / 2, "Robot");
+    player.anims.create({
+      key: "attack",
+      frames: player.anims.generateFrameNames("Robot", {
+        start: 0,
+        end: 2,
+        prefix: "character_robot_attack",
+        suffix: ".png"
+      }),
+      frameRate: 1200,
+      repeat: -1,
+    });
+    player.play("attack")
   }
   update() {}
 }
