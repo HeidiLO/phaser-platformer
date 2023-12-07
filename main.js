4b6ca645ff4a183acfee718e4a8b9e51f2031415
Heidi <osburnhl@s.dcsdk12.org>
2023-12-07 15:44:20 -0700

    5-12-2023

--- main.js
+++ main.js
@@ -31,12 +31,19 @@ class MainScene extends Phaser.Scene {
     this.load.audio("coin-noise", "coin.mp3");
     this.load.audio("jump-noise", "jump.wav");
     this.load.audio("noise", "background-music.mp3");
+    this.load.image("spike", "spikeBall.png");
+    this.load.audio("dead", "die.mp3");
+    this.load.audio("music", "music.mp3");
   }
   create() {
     let playerSpawn = {
       x: WIDTH /2,
       y: HEIGHT/2,
     };
+    let spikeSpawn = {
+      x: WIDTH /3,
+      y: HEIGHT /3,
+    }
     this.physics.world.setBounds(0,0,WIDTH, HEIGHT);
     this.coinNoise = this.sound.add("coin-noise", {
       volume: 0.5
@@ -44,11 +51,19 @@ class MainScene extends Phaser.Scene {
     this.jumpNoise = this.sound.add("jump-noise",{
       volume: 0.5
     });
+    this.dead = this.sound.add("dead",{
+      volume: 2,
+    })
     this.noise = this.sound.add("noise",{
+      loop: true,
+      volume: 0.5
+    });
+    this.music = this.sound.add("music",{
       loop: true,
       volume: 0.75
     });
     this.noise.play();
+    this.music.play();
     const { height, width } = this.scale;
     this.map = this.make.tilemap({ key: "map" });
     const objectLayer = this.map.getObjectLayer("objects");
@@ -58,6 +73,14 @@ class MainScene extends Phaser.Scene {
         case"player-spawn":
         playerSpawn.x = x+width/2;
         playerSpawn.y = y + height/2
+      }
+    })
+    objectLayer.objects.forEach((o)=>{
+      const{x = 0, y = 0, name, width = 0, height = 0} = o;
+      switch(name){
+        case"enemy-spawn":
+        spikeSpawn.x = x+width/4;
+        spikeSpawn.y = y + height/4;
       }
     })
     const marbleTiles = this.map.addTilesetImage("marble", "Marble");
@@ -112,6 +135,29 @@ class MainScene extends Phaser.Scene {
       repeat: -1,
     });
     this.player.play("run");
+    this.spike = this.physics.add.group({
+      key: "spike",
+      quantity: 4,
+      setXY: {x: 18 * 4, y: 0, stepX: 18*3},
+      setScale: {x: 0.15, y: 0.15},
+    });
+    this.spike.children.iterate((coin)=>{
+      coin
+        .setCircle(40)
+        .setCollideWorldBounds(true)
+        .setBounce(Phaser.Math.FloatBetween(0.2, 0.5))
+        .setVelocityX(Phaser.Math.FloatBetween(-5,5));
+    })
+    this.physics.add.collider(this.spike, platformLayer);
+    this.physics.add.overlap(
+      this.player,
+      this.spike,
+      this.die,
+      undefined,
+      this
+    )
+    this.physics.add.collider(this.coins, this.spike);
+    this.physics.add.collider(this.spike, this.spike);
     this.cursors = this.input.keyboard.addKeys({
       left: Phaser.Input.Keyboard.KeyCodes.A,
       leftArrow: Phaser.Input.Keyboard.KeyCodes.LEFT,
@@ -177,6 +223,10 @@ class MainScene extends Phaser.Scene {
  collectCoin(player, coin){
     this.coinNoise.play();
    coin.disableBody(true, true);
+  }
+  die(player, spike){
+    this.dead.play();
+   player.disableBody(true, true);
   }
 }
 /**@type {Phaser.Types.Core.GameConfig}*/
